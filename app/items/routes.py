decfd9125c45b402d25631e1cbb2da2abb2283ac
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2020-02-14 21:47:42 +0000

    Added extra columns to generate a history of items

--- app/items/routes.py
+++ app/items/routes.py
@@ -1,4 +1,7 @@
+import datetime
+
 from flask import jsonify, render_template, request, flash, redirect, url_for, abort
+
 
 from app.items import bp
 from app.items.forms import AddUpdateItemForm
@@ -15,7 +18,7 @@ def add_item():
     form = AddUpdateItemForm()
     if request.method == 'POST':
         data = request.form
-        i = Item(name=data['name'], description=data['description'], owner=data['owner'])
+        i = Item(name=data['name'], description=data['description'], owner=data['owner'],created=datetime.datetime.utcnow())
         db.session.add(i)
         db.session.commit()
         flash('Item created.')
@@ -38,7 +41,7 @@ def update_item(item_id):
         i.update(dict(name=data['name'],owner=data['owner'],description=data['description']))
         db.session.commit()
         return redirect(url_for('items.view_items'))
-    return render_template('updateItem.html',form=form)
+    return render_template('updateItem.html',form=form, item=i)
 
 @bp.route('/delete/<item_id>', methods=['GET','DELETE'])
 def delete_item(item_id):
@@ -46,3 +49,8 @@ def delete_item(item_id):
     db.session.delete(i)
     db.session.commit()
     return redirect(url_for('items.view_items'))
+
+@bp.route('/history/<item_id>',methods=['GET'])
+def view_history(item_id):
+    items = []
+    i = Item.query.filter_by(id=item_id).first_or_404()
--- app/models.py
+++ app/models.py
@@ -1,11 +1,31 @@
 from app import db 
 
-
 class Item(db.Model):
+    __tablename__ = 'item'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     description = db.Column(db.String(255))
     owner = db.Column(db.String(120))
+    created = db.Column(db.DateTime())
+    archived = db.Column(db.DateTime())
+    previous_version_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    newer_version = db.relationship('Item',backref='previous_version',lazy=True,remote_side=[id],uselist=False)
+    is_archived = False
+    
+    def archive(self):
+        self.is_archived = True
+    
+    def has_previous(self):
+        return (self.previous_version is not None)
+        
+    def is_latest(self):
+        return (self.newer_version is None)
+
+    def update_item(self, newer_version):
+        self.archive = True
+        self.newer_version =  newer_version
+        newer_version.previous_version = self
+
 
     def __repr__(self):
         return f'<Item {self.id} : {self.name}>'
